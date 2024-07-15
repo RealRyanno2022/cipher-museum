80d853503dd757cb90f3a77af4a1ffb13d5b591a
RealRyanno2022 <ryannobusiness@gmail.com>
2024-07-15 12:52:18 +0100

    added sidebars

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import HistoryPage from './pages/HistoryPage';
-
+import './background.css';
 
 const App: React.FC = () => {
   return (
-   <div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/history" element={<HistoryPage />} />
-          </Routes>
-        </Router>
+    <div className="background-wrapper">
+      <div className="animated-background"></div>
+      <div className="content">
+        <div className="wrapper">
+          <Router>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/history" element={<HistoryPage />} />
+            </Routes>
+          </Router>
+        </div>
+      </div>
     </div>
   );
 };
